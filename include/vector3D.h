45475696f93752ad24b06e4a002142babbcc0627
Baretop <62176508+Baretop@users.noreply.github.com>
2020-03-15 17:02:31 +0300

    Update vector3D.h

--- include/vector3D.h
+++ include/vector3D.h
@@ -1,5 +1,6 @@
 #ifndef GASSIMULATION_VECTOR3D_H
 #define GASSIMULATION_VECTOR3D_H
+
 #include <iostream>
 #include <cmath>
 
@@ -12,158 +13,40 @@ private:
     double z;
 
 public:
-    vector3D()
-    {
-        x = 0;
-        y = 0;
-        z = 0;
-    }
+    vector3D();
+    vector3D(double x, double y, double z);
+        
+    void setX (double x);
+    void setY(double y);
+    void setZ(double z);
+        
+    double getX();
+    double getY();
+    double getZ();
 
-    vector3D(double x, double y, double z)
-    {
-        this->x = x;
-        this->y = y;
-        this->z = z;
-    }
+    vector3D operator+(const vector3D& b);
+    vector3D operator-(const vector3D& b);
+    vector3D operator*(const double& b);
+    vector3D operator/(const double& b);
+        
+    double operator*(const vector3D& b); //scalar multiplication: *
+    vector3D operator^(const vector3D& b); //vector multiplication: ^
+        
+    bool operator==(const vector3D& b);
+    bool operator!=(const vector3D& b);
 
-    void setX (double x)
-    {
-        this->x = x;
-    }
+    vector3D operator+= (const vector3D &b);
+    vector3D operator-= (const vector3D &b);
+    vector3D operator*= (const double &b);
+    vector3D operator/= (const double &b);
 
-    void setY(double y)
-    {
-        this->y = y;
-    }
-
-    void setZ(double z)
-    {
-        this->z = z;
-    }
-
-    double getX()
-    {
-        return x;
-    }
-
-    double getY()
-    {
-        return y;
-    }
-
-    double getZ()
-    {
-        return z;
-    }
-
-    vector3D operator+(const vector3D& b)
-    {
-        vector3D result = {x + b.x, y + b.y, z + b.z};
-        return result;
-    }
-
-    vector3D operator-(const vector3D& b)
-    {
-        vector3D result = {x - b.x, y - b.y, z - b.z};
-        return result;
-    }
-
-    vector3D operator*(const double& b)
-    {
-        vector3D result = {x * b, y * b, z * b};
-        return result;
-    }
-
-    vector3D operator/(const double& b)
-    {
-        vector3D result = {x / b, y / b, z / b};
-        return result;
-    }
-
-    //scalar multiplication: *
-    double operator*(const vector3D& b)
-    {
-        double result = x * b.x + y * b.y + z * b.z;
-        return result;
-    }
-
-    //vector multiplication: ^
-    vector3D operator^(const vector3D& b)
-    {
-        vector3D result = {y * b.z - b.y * z, z * b.x - b.z * x, x * b.y - y * b.x};
-        return result;
-    }
-
-    bool operator==(const vector3D& b)
-    {
-        if(x == b.x && y == b.y && z == b.z)
-            return true;
-        else
-            return false;
-    }
-
-    bool operator!=(const vector3D& b)
-    {
-        if(x == b.x && y == b.y && z == b.z)
-            return false;
-        else
-            return true;
-    }
-
-    vector3D operator+= (const vector3D &b)
-    {
-        x += b.x;
-        y += b.y;
-        z += b.z;
-        return *this;
-    }
-
-    vector3D operator-= (const vector3D &b)
-    {
-        x -= b.x;
-        y -= b.y;
-        z -= b.z;
-        return *this;
-    }
-
-    vector3D operator*= (const double &b)
-    {
-        x *= b;
-        y *= b;
-        z *= b;
-        return *this;
-    }
-
-    vector3D operator/= (const double &b)
-    {
-        x /= b;
-        y /= b;
-        z /= b;
-        return *this;
-    }
-
-    // vector's length:
-    double length()
-    {
-        return sqrt(x * x + y * y + z * z);
-    }
+    double length();
 };
 
-vector3D operator*(const double& b, vector3D& a)
-{
-    vector3D result = {a.getX() * b, a.getY() * b, a.getZ() * b};
-    return result;
-}
+vector3D operator*(const double& b, vector3D& a);
 
-std::ostream& operator<<(std::ostream& out, vector3D a)
-{
-    out << "("<<a.getX()<<", "<<a.getY()<<", "<<a.getZ()<<")";
-    return out;
-}
+std::ostream& operator<<(std::ostream& out, vector3D a);
 //use () to display vector product of a, b
 //example: cout << (a^b);
-
-
-
 
 #endif //GASSIMULATION_VECTOR3D_H
