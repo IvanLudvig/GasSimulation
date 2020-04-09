770d5b4c30cd38d771f28f8f88a643f4924d29f4
Baretop <62176508+Baretop@users.noreply.github.com>
2020-04-09 23:04:08 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    Gas gas(1000, 1, vector3D(100, 100, 100), 1, 1);
+    Gas gas(1000, 1, vector3D(100, 100, 100), 1, 1, 20);
     for (int i = 0; i < 2000; i++)
     {
         gas.update();
